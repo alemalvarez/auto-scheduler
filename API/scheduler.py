33ee095d55debb9e58ba80a80429495bbeeb3e2d
Alejandro Mata <alemalvarez@icloud.com>
2024-03-12 11:16:20 -0700

    Make the scheduler flexible toward capitalization, update the scraper to export capitalized course names

--- API/scheduler.py
+++ API/scheduler.py
@@ -1,6 +1,9 @@
 import os
 import json
 import ast
+import glob
+import re
+
 from openai import OpenAI
 from rich.console import Console
 from rich.markdown import Markdown
@@ -29,9 +32,11 @@ def get_timetables(requirements, schedule):
     """
     timetables = {}
     for requirement in requirements:
-        major = requirement.split(' ')[0].upper()
-        if major in schedule and requirement in schedule[major]:
-            timetables[requirement] = schedule[major][requirement]
+        major = re.match(r"^[^\d\s]+", requirement).group(0).upper()
+        course = re.sub(r'\s+', '', requirement).upper()
+
+        if major in schedule and course in schedule[major]:
+            timetables[course] = schedule[major][course]
         else:
             print(f"No timetable found for requirement: {requirement}")
     return timetables
@@ -40,9 +45,10 @@ def query_openai_api(requirements, timetables):
     """
     Query the OpenAI API to get a non-overlapping combination of timeslots.
     """
+
     instructions = """
     You are a scheduler. You make college schedules. When you are told a list of classes and their days and hours, you find a combination of the possible timeslots
-    avoiding any overlap. 
+    avoiding any overlap. For each class, return one and only one timeslot.
     """
     prompt = f"""
     From this list of classes, pick a non-overlapping combination of timeslots. All classes need to be taken strictly once.
@@ -66,12 +72,17 @@ def main():
     """
     Main function to run the program.
     """
-    term = input("Enter the term to target (e.g., 'fall-2023', 'spring-2024'): ")
+    # Print the possible schedules
+    print("Found schedules:")
+    for filename in glob.glob('../scraper/schedules/*.json'):
+        print(os.path.basename(filename).split('.')[0])
+
+    term = input("Enter the term to target: ")
+
     schedule = load_schedule(term)
-    if schedule is not None:
-        requirements = get_requirements()
-        timetables = get_timetables(requirements, schedule)
-        query_openai_api(requirements, timetables)
+    requirements = get_requirements()
+    timetables = get_timetables(requirements, schedule)
+    query_openai_api(requirements, timetables)
 
 if __name__ == "__main__":
     main()
